a7ee58a6ce4228a0768f4d175c52ef7bec94e587
Chris Hoge <chris@openstack.org>
2017-06-22 12:00:03 -0700

    Added Ironic Conductor

---
+++ 45-ironic-conductor.sh
@@ -0,0 +1,10 @@
+# Starts the Ironic Conductor service
+docker run -d \
+           -v ironic-imagedata-volume:/imagedata \
+           --env-file ./config \
+           --hostname ironic-conductor \
+           --name ironic-conductor \
+           --link=rabbitmq:rabbitmq \
+           --link=mariadb:mariadb \
+           -p 3260:3260 \
+           ironic-conductor:centos
