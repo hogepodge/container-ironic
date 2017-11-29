c36b18593ac6bb4c175f2bb5d085b008ddaad01a
Chris Hoge <chris@openstack.org>
2017-11-29 12:49:40 -0800

    Neutron metadata agent

--- neutron/base/generate.configs.sh
+++ neutron/base/generate.configs.sh
@@ -1,3 +1,4 @@
 /generate.linuxbridge_agent.ini
 /generate.metadata_agent.ini
 /generate.neutron.conf
+/generate.metadata_agent.ini
--- neutron/metadata-agent/start-neutron-metadata-agent.sh
+++ neutron/metadata-agent/start-neutron-metadata-agent.sh
@@ -4,5 +4,5 @@ neutron-metadata-agent \
     --config-file /etc/neutron/neutron.conf \
     --config-file /etc/neutron/plugins/ml2/ml2_conf.ini \
     --config-file /etc/neutron/plugins/ml2/linuxbridge_agent.ini \
-    --config-file /ets/neutron/dhcp_agent.ini \
+    --config-file /etc/neutron/dhcp_agent.ini \
     --config-file /etc/neutron/metadata_agent.ini
